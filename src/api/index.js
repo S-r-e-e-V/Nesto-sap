62d76d1c517a63df186e3be7bd0a71ea451e8649
Sree <vsree@riafy.me>
2021-10-12 16:33:31 +0530

    hardcoded base URL

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,8 @@ import {
   getStageStringPrefix,
 } from "./networkUtils";
 
-const API_BASE_URL = process.env.REACT_APP_URL;
+// const API_BASE_URL = process.env.REACT_APP_URL;
+const API_BASE_URL = "https://dev-api.nesto.shop";
 
 const AUTH_BASE_URL = "https://auth.nesto.shop" + getStageString(API_BASE_URL);
 
